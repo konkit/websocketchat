500afe894cfcd03a38d69d9c5cfe6bae9fd0c0bc
Lukasz Tenerowicz <lukasz.tenerowicz@polcode.net>
2015-09-08 13:30:30 +0200

    Added RVM settings to capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,6 +36,8 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
+set :default_env, { rvm_bin_path: '~/.rvm/bin' }
+
 namespace :deploy do
 
   after :restart, :clear_cache do
