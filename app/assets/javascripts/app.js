03f97d791d66b8598fa6c4a71f49d0cbcea7f922
Lukasz Tenerowicz <lukasz.tenerowicz@polcode.net>
2015-09-09 14:17:26 +0200

    Added different templates for different auth options

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -11,11 +11,39 @@ websocketchat.config([
   '$urlRouterProvider',
   function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.when('', '/');
+    $urlRouterProvider.when('/', '/login_temp');
 
     $stateProvider.state('app', {
-      url: '/',
+      abstract: true,
       templateUrl: 'templates/home.html',
       controller: 'HomeController',
+    })
+    .state('app.login_temp_username', {
+      url: '/login_temp',
+      views: {
+        'authenticate': {
+          templateUrl: 'templates/login_temp_username.html',
+          controller: 'HomeController',
+        }
+      }
+    })
+    .state('app.login_devise_user', {
+      url: '/login_devise/',
+      views: {
+        'authenticate': {
+          templateUrl: 'templates/login_devise_user.html',
+          controller: 'HomeController',
+        }
+      }
+    })
+    .state('app.login_facebook', {
+      url: '/login_facebook/',
+      views: {
+        'authenticate': {
+          templateUrl: 'templates/login_facebook.html',
+          controller: 'HomeController',
+        }
+      }
     })
   }
 ]);
