6afd0310f8f557666cb0556116efca7c7f7c2d15
Lukasz Tenerowicz <lukasz.tenerowicz@polcode.net>
2015-09-09 11:19:24 +0200

    Added chat message type

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,3 +25,12 @@
 //= require services/services
 
 //= require_tree .
+
+String.prototype.supplant = function (o) {
+  return this.replace(/{([^{}]*)}/g,
+    function (a, b) {
+        var r = o[b];
+        return typeof r === 'string' || typeof r === 'number' ? r : a;
+    }
+  );
+};
--- app/controllers/chat_websocket_controller.rb
+++ app/controllers/chat_websocket_controller.rb
@@ -1,22 +1,47 @@
 class ChatWebsocketController < WebsocketRails::BaseController
   def initialize_session
-    controller_store[:message_count] = 0
     controller_store[:users] = []
   end
 
   def add_message
-    chat_message = "[#{Time.now.strftime('%H:%M:%S')}] #{message[:user]} > #{message[:text]}"
-    broadcast_message :chat_listener, chat_message
+    send_simple_message
   end
 
   def add_user
     controller_store[:users] << message[:username]
     connection_store[:current_user] = message[:username]
     broadcast_message(:users_list_listener, controller_store[:users])
+    send_user_joined_message
   end
 
   def delete_user
     controller_store[:users].delete( connection_store[:current_user] )
     broadcast_message(:users_list_listener, controller_store[:users])
+    send_user_left_message
+  end
+
+  private
+  def send_simple_message
+    chat_message = {
+      type: :message,
+      text: "[#{Time.now.strftime('%H:%M:%S')}] #{message[:user]} > #{message[:text]}"
+    }
+    broadcast_message :chat_listener, chat_message
+  end
+
+  def send_user_joined_message
+    sent_message = {
+      type: :info,
+      text: "[#{Time.now.strftime('%H:%M:%S')}] User #{connection_store[:current_user]} joined"
+    }
+    broadcast_message(:chat_listener, sent_message)
+  end
+
+  def send_user_left_message
+    sent_message = {
+      type: :info,
+      text: "[#{Time.now.strftime('%H:%M:%S')}] User #{connection_store[:current_user]} left"
+    }
+    broadcast_message(:chat_listener, sent_message)
   end
 end
